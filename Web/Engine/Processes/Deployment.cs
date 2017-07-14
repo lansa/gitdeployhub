3069b3082433d80b3ed896e6aa90a8f1f6f6ca25
Rob Goodridge <rob.goodridge@lansa.com.au>
2017-07-14 14:08:02 +1000

    Performance updates and new binary

--- Web/Engine/Processes/Deployment.cs
+++ Web/Engine/Processes/Deployment.cs
@@ -19,27 +19,19 @@ namespace GitDeployHub.Web.Engine.Processes
 
             // Doing a Fetch and a Pull causes 2 connections to the origin to be made. This is far slower than the rest of the 
             // processing, and is a special case anyway, as this task will usually be caused by a commit to origin
-            // base.DoExecute();
-            //if (Instance.FilesChangedToTreeish.Length > 0)
-            if ( true )
+            if (!Dry)
             {
-                if (!Dry)
-                {
-                    Instance.ExecutePreDeploy(this);
-                    Instance.ResetHard(this);         // Clean out all local changes from the directory, there should be none and the 1st install via MSI leaves files in the Package Directory, of course
-                    Instance.Pull(this);             
-                    Instance.ExecutePostDeploy(this);
-                    Log("Instance Deployed: " + Instance.Name);
-                    Instance.LastDeployment = this;
-                }
+               Instance.ExecutePreDeploy(this);
+               Instance.ResetHard(this);         // Clean out all local changes from the directory, there should be none and the 1st install via MSI leaves files in the Package Directory, of course
+               Instance.Pull(this);             
+               Instance.ExecutePostDeploy(this);
+               Log("Instance Deployed: " + Instance.Name);
+               Instance.LastDeployment = this;
             }
             else
             {
-                Skipped = true;
-                Log("Skipping deployment.");
-                LogNewLine();
+               Instance.Fetch(this);
             }
         }
-
     }
 }
