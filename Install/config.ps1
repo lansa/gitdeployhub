5004d9f4a0a8ebbd12952107db1e490a81e7422a
Rob Goodridge <rob.goodridge@lansa.com.au>
2017-09-25 16:20:53 +1000

    158561 Add a target instance to Git Deploy Hub configuration

--- Install/config.ps1
+++ Install/config.ps1
@@ -3,9 +3,31 @@
 
 Configure GitDeployHub for a single target LANSA environment
 
+Presumed to be running from within the website that is being configured
+
 .EXAMPLE
 
+.\Install\Config.ps1 MyWebSite
+
 #>
+
+param (
+    [Parameter(Mandatory=$true)]
+        [string]
+        $WebSiteName,
+
+    [Parameter(Mandatory=$true)]
+        [String] 
+        $folder,
+        
+    [Parameter(Mandatory=$true)]
+        [String] 
+        $APPL,
+        
+    [Parameter(Mandatory=$false)]
+        [Boolean] 
+        $AddConfig = $true
+)
 
 #Requires -RunAsAdministrator
 
@@ -20,3 +42,30 @@ if ( -not ($ExecutionPolicy -eq 'Unrestricted' -or $ExecutionPolicy -eq 'Bypass'
     }        
 }
 
+$MyInvocation.MyCommand.Path
+$ScriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+$GitRepoRoot = Split-Path -Parent $ScriptDir
+$WebSiteRootPath = Join-Path $GitRepoRoot 'web'
+$Config = Join-Path $WebSiteRootPath 'web.config'
+
+$doc = New-Object System.Xml.XmlDocument
+$doc.Load($Config)
+
+$Instance = $doc.SelectSingleNode('configuration/gitDeployHub/instances/instance[@name="' + $WebSiteName + '"]')
+
+if ( $AddConfig) {
+    if ( $Instance -eq $null ) {
+        $Instance = $doc.configuration.gitDeployHub.instances.AppendChild($doc.CreateElement("instance"));
+        $Instance.SetAttribute("name",$WebSiteName);
+    }
+    $Instance.SetAttribute("folder",$folder);
+    $Instance.SetAttribute("projectFolder", "X_Win95\X_Lansa\X_Apps\$APPL");
+    $Instance
+} else {
+    if ( $Instance -eq $null ) {
+        Write-Error ("Instance $WebSiteName does not exist")
+    } else {
+        $Instance.ParentNode.RemoveChild($Instance)
+    }
+}
+$doc.Save($Config)
