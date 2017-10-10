147dcf288fac7409715c800260b5c5e5d889479d
Rob Goodridge <rob.goodridge@lansa.com.au>
2017-10-11 07:25:19 +1100

    158561 Install and Configure GitDeployHub

--- Install/config.ps1
+++ Install/config.ps1
@@ -29,38 +29,76 @@ param (
         $AddConfig = $true
 )
 
-#Requires -RunAsAdministrator
-
-$TargetSystem
-$folder
-$APPL
-$AddConfig
-
-$MyInvocation.MyCommand.Path
-$ScriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
-$GitRepoRoot = Split-Path -Parent $ScriptDir
-$WebSiteRootPath = Join-Path $GitRepoRoot 'web'
-$Config = Join-Path $WebSiteRootPath 'web.config'
-
-$doc = New-Object System.Xml.XmlDocument
-$doc.Load($Config)
-
-$Instance = $doc.SelectSingleNode('configuration/gitDeployHub/instances/instance[@name="' + $TargetSystem + '"]')
-$Instances = $doc.SelectSingleNode('configuration/gitDeployHub/instances')
-
-if ( $AddConfig) {
-    if ( $Instance -eq $null ) {
-        $Instance = $Instances.AppendChild($doc.CreateElement("instance"));
-        $Instance.SetAttribute("name",$TargetSystem);
-    }
-    $Instance.SetAttribute("folder",$folder);
-    $Instance.SetAttribute("projectFolder", "X_Win95\X_Lansa\X_Apps\$APPL");
-    $Instance
-} else {
-    if ( $Instance -eq $null ) {
-        Write-Error ("Instance $TargetSystem does not exist")
-    } else {
-        $Instance.ParentNode.RemoveChild($Instance)
-    }
+function Log-Date 
+{
+    ((get-date).ToUniversalTime()).ToString("yyyy-MM-dd HH:mm:ssZ")
 }
-$doc.Save($Config)
+
+cmd /c exit 0    #Set $LASTEXITCODE
+
+try {
+    #Requires -RunAsAdministrator
+
+    $MyInvocation.MyCommand.Path
+    $ScriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+    $GitRepoRoot = Split-Path -Parent $ScriptDir
+    $WebSiteRootPath = Join-Path $GitRepoRoot 'web'
+    $Config = Join-Path $WebSiteRootPath 'web.config'
+
+    Write-Output("$(Log-Date)")
+
+    Write-Output ("TargetSystem: $TargetSystem")
+    Write-Output ("Folder: $folder")
+    Write-Output ("APPL: $APPL")
+    Write-Output ("AddConfig: $AddConfig")
+
+    $doc = New-Object System.Xml.XmlDocument
+    $doc.Load($Config)
+
+    $Instance = $doc.SelectSingleNode('configuration/gitDeployHub/instances/instance[@name="' + $TargetSystem + '"]')
+    $Instances = $doc.SelectSingleNode('configuration/gitDeployHub/instances')
+
+    if ( $AddConfig) {
+        if ( $Instance -eq $null ) {
+            $Instance = $Instances.AppendChild($doc.CreateElement("instance"));
+            $Instance.SetAttribute("name",$TargetSystem);
+        }
+        $Instance.SetAttribute("folder",$folder);
+        $Instance.SetAttribute("projectFolder", "X_Win95\X_Lansa\X_Apps\$APPL");
+        $Instance
+    } else {
+        if ( $Instance -eq $null ) {
+            Write-Error ("Instance $TargetSystem does not exist")
+        } else {
+            $Instance.ParentNode.RemoveChild($Instance)
+        }
+    }
+    $doc.Save($Config)
+} catch {
+    $e = $_.Exception
+    $e|format-list -force
+    
+    Write-Output("InstallConfigurationation failed")
+    Write-Output("Raw LASTEXITCODE $LASTEXITCODE")  
+    if ( (-not [string]::IsNullOrWhiteSpace($LASTEXITCODE)) -and ($LASTEXITCODE -ne 0)) {
+        $ExitCode = $LASTEXITCODE
+        Write-Output("ExitCode set to LASTEXITCODE $ExitCode")        
+    } else {
+        $ExitCode =  $e.HResult
+        Write-Output("ExitCode set to HResult $ExitCode")        
+    }
+
+    if ( $ExitCode -eq $null -or $ExitCode -eq 0 ) {
+        $ExitCode = -1
+        Write-Output("ExitCode set to $ExitCode")        
+    }
+    Write-Output("Final ExitCode $ExitCode")
+    cmd /c exit $ExitCode    #Set $LASTEXITCODE
+    Write-Output("Final LASTEXITCODE $LASTEXITCODE")
+    return    
+} finally {
+    Write-Output("$(Log-Date)")
+}
+Write-Output("Configuration succeeded")
+cmd /c exit 0    #Set $LASTEXITCODE
+Write-Output("LASTEXITCODE $LASTEXITCODE")
--- Install/uninstall.ps1
+++ Install/uninstall.ps1
@@ -1,7 +1,7 @@
 <#
 .SYNOPSIS
 
-Install GitDeployHub
+UnInstall GitDeployHub
 
 This script presumes that the GitDeployHub git repo has been cloned into a directory of this form:
 c:\inetpub\wwwroot_git\GitDeployHub. Though it should not make any difference
@@ -18,18 +18,13 @@ this command first before the script may be executed
 powershell.exe -ExecutionPolicy Bypass .\install.ps1
 
 to specify the web site name and port number
-powershell.exe -ExecutionPolicy Bypass .\install.ps1 -WebSiteName 'testgit' -WebSitePort '8099'
-
+powershell.exe -ExecutionPolicy Bypass .\uninstall.ps1 -WebSiteName 'testgit'
 #>
 
 param (
     [Parameter(Mandatory=$false)]
         [string]
-        $WebSiteName,
-
-    [Parameter(Mandatory=$false)]
-        [string]
-        $WebSitePort = 8090 
+        $WebSiteName
 )
 
 function Log-Date 
@@ -47,79 +42,56 @@ try {
     $MyInvocation.MyCommand.Path
     $ScriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
     $GitRepoRoot = Split-Path -Parent $ScriptDir
-    $WebSiteRootPath = Join-Path $GitRepoRoot 'web'
 
     Write-Output("$(Log-Date)")
 
     Write-Output ("WebSiteName: $WebSiteName")
-    Write-Output ("WebSitePort: $WebSitePort")
-    
+        
     # Default install location is c:\inetpub\wwwroot_git
     # LANSA Path is c:\program files(x86)\mycompany\myapp\tools
     # If its the default path, call the website 'git'
     # If its the LANSA path, use 'myapp'
 
+    $IsDefault = $false
+
     if ( [string]::IsNullOrWhiteSpace($WebSiteName) ) {
         $TempName = split-path -Parent $GitRepoRoot | split-path -Parent | split-path -Leaf
         if ( $TempName -eq 'inetpub') {
             $WebSiteName = 'git'
+            $IsDefault = $true
         } else {
             $WebSiteName = $TempName
         }
     }
 
     Write-Output ("Web Site Name = $WebSiteName")
-
-    # Add the IIS feature ASP 4.x.
-    $ASPNET45 = Get-WindowsOptionalFeature -Online | Where-Object {$_.state -eq "Enabled" -and $_.FeatureName -eq 'IIS-ASPNET45'}
-    if ( $ASPNET45 -eq $null ) {
-        Write-Output("Enabling ASP.NET 4.5")
-        Enable-WindowsOptionalFeature -Online -FeatureName IIS-ASPNET45
-        Write-Output("Finished enabling ASP.NET 4.5")
-    }
-
-    # Create a new web site in c:\inetpub\wwwroot_git and set the port number to 8090.
     $HubSite = Get-ChildItem iis:\Sites | Where-Object{$_.Name -eq $WebSiteName}
-    $HubSite
-    if ( $HubSite -eq $null) {
-        New-Item iis:\Sites\$WebSiteName -bindings @{protocol="http";bindingInformation="*:$($WebSitePort):"} -physicalPath $WebSiteRootPath -Force 
+    if ( $HubSite -ne $null) {
 
-        # Create an App Pool
-
-        $AppPool = Get-ChildItem iis:\AppPools | Where-Object{$_.Name -eq $WebSiteName}
-        if ( $AppPool -eq $null ) {
-            new-item iis:\AppPools\$WebSiteName
+        # Stop web site provided iis server is running
+        $iis = get-wmiobject Win32_Service -Filter "name='w3svc'"
+        if ( $iis -ne $null -and $iis.state -eq 'Running') {
+            Stop-Website -name $WebSiteName
+        }        
+        
+        Write-Output ("Delete a hub application in $WebSiteName web site")
+        Remove-WebApplication -Name "Hub" -Site $WebSiteName
+        
+        if ( $IsDefault ) {
+            # Delete the web site in c:\inetpub\wwwroot_git
+            $HubSite = Get-ChildItem iis:\Sites | Where-Object{$_.Name -eq $WebSiteName}
+            if ( $HubSite -ne $null ) {
+                Remove-Item $HubSite
+            }
         }
-
-        # Set the App Pool user to LocalSystem. Advanced Settings\Identity:
-        # So that Git Deploy Hub has permission to use the ssh file
-        #$AppPool.processModel.identityType = "LocalSystem"
-        #$AppPool | set-Item
-
-        # Associate website with App Pool
-        Set-ItemProperty IIS:\Sites\$WebSiteName -name applicationPool -value $WebSiteName
-    } 
-
-    Write-Output ("Create a hub application in $WebSiteName web site")
-    New-WebApplication -Name "Hub" -Site $WebSiteName -PhysicalPath $WebSiteRootPath -ApplicationPool $HubSite.applicationPool -Force
-
-    # Set the default git environment to be the currently logged on user so that the same SSH key is used for system processes.
-    # Set the HOME environment SYSTEM variable to the current users home directory:
-
-    $HOME2 = Join-Path $ENV:HOMEDRIVE $ENV:HOMEPATH
-    Write-Output ("Home environment $HOME2")
-    [Environment]::SetEnvironmentVariable('HOME', $HOME2 , 'Machine')
-
-    # Start web site provided iis server is running
-    $iis = get-wmiobject Win32_Service -Filter "name='w3svc'"
-    if ( $iis -ne $null -and $iis.state -eq 'Running') {
-        Start-Website -name $WebSiteName
+    } else {
+        Write-Output("Warning: web site does not exist")
     }
 } catch {
     $e = $_.Exception
     $e|format-list -force
     
-    Write-Output("Installation failed")
+    Write-Output("Uninstallation failed")
     Write-Output("Raw LASTEXITCODE $LASTEXITCODE")  
     if ( (-not [string]::IsNullOrWhiteSpace($LASTEXITCODE)) -and ($LASTEXITCODE -ne 0)) {
         $ExitCode = $LASTEXITCODE
@@ -140,6 +112,6 @@ try {
 } finally {
     Write-Output("$(Log-Date)")
 }
-Write-Output("Installation succeeded")
+Write-Output("Uninstallation succeeded")
 cmd /c exit 0    #Set $LASTEXITCODE
 Write-Output("LASTEXITCODE $LASTEXITCODE")
